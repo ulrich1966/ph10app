d989f50ad99303f7989ef9813827808114cb2feb
Uli <post2uli@gmx.de>
2019-03-15 11:42:58 +0100

    inflate build gradle

---
+++ app/src/main/java/de/auli/ph10zettel/util/ApiUrl.java
@@ -0,0 +1,11 @@
+package de.auli.ph10zettel.util;
+
+public class ApiUrl {
+	public static final String BASE = "http://localhost:8080/ph10zettel/api";
+	public static final String REMOTE_BASE = "http://h2825659.stratoserver.net/ph10zettel/api";
+	public static final String GROUPS = "%s/playergroup";
+	public static final String GROUP_ID = GROUPS+"/%s";
+	public static final String PLAYER = "%s/player";
+	public static final String PLAYER_ID = PLAYER+"/%s";
+	public static final String GROUP_ADD_PLAYER = "%s/playergroup/%s/player/%s";
+}
