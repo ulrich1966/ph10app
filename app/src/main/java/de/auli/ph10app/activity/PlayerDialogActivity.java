6115b76f088d7bbecb2f575498902f1f0fb6f805
ulrich1966 <post2uli@gmx.de>
2019-03-21 07:26:20 +0100

    dialog activity

--- app/src/main/java/de/auli/ph10app/activity/PlayerDialogActivity.java
+++ app/src/main/java/de/auli/ph10app/activity/PlayerDialogActivity.java
@@ -1,17 +1,24 @@
 package de.auli.ph10app.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 
 import de.auli.ph10app.R;
 import de.auli.ph10app.fragment.PlayerDialogFragment;
 import de.auli.ph10app.util.AppLogger;
 
-public class PlayerDialogActivity extends Activity {
+public class PlayerDialogActivity extends FragmentActivity implements PlayerDialogFragment.NoticeDialogListener {
     private static final AppLogger LOG = new AppLogger(PlayerDialogActivity.class, false);
     private View rootView;
     private PlayerDialogFragment plsDialogFragment = new PlayerDialogFragment();
+
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return super.getSupportFragmentManager();
+    }
 
     // Activity needs defaultconstructor
     public PlayerDialogActivity() {
@@ -23,10 +30,29 @@ public class PlayerDialogActivity extends Activity {
         this.rootView = rootView;
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DialogFragment dialog = new PlayerDialogFragment();
+        dialog.show(getSupportFragmentManager(), "PlayerDialogFragment");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_dialog);
     }
 
+
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+
+    }
 }
--- app/src/main/java/de/auli/ph10app/fragment/PlayerDialogFragment.java
+++ app/src/main/java/de/auli/ph10app/fragment/PlayerDialogFragment.java
@@ -1,9 +1,14 @@
 package de.auli.ph10app.fragment;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,11 +25,65 @@ public class PlayerDialogFragment extends DialogFragment {
         // Required empty public constructor
     }
 
+    public interface NoticeDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener listener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(getActivity().toString() + " must implement NoticeDialogListener");
+        }
+    }
+
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Build the dialog and set up the button click handlers
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.head_playerlist).setPositiveButton(R.string.cmd_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the positive button event back to the host activity
+                        listener.onDialogPositiveClick(PlayerDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.cmd_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the negative button event back to the host activity
+                        listener.onDialogNegativeClick(PlayerDialogFragment.this);
+                    }
+                });
+        return builder.create();
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player_dialog2, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_player_dialog, container, false);
+        return rootView;
     }
 
+    @Override
+    public void show(FragmentManager manager, String tag) {
+        super.show(manager, tag);
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+    }
 }
